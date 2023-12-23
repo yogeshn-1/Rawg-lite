6da169c99f2e72bdc9f4f9a15d947063aff6db90
yogeshn-1 <nirmalkary@gmail.com>
2023-12-23 22:38:40 +0530

    Removing default icon of vite

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,15 +1,11 @@
 import { HStack, Image } from "@chakra-ui/react";
 import icon from "../assets/icon.png";
-import { useEffect } from "react";
 import ColorSwitch from "./ChangeColorScheme";
 import SearchBox from "./SearchBox";
 interface Prop {
   onSearch: (searchText: string) => void;
 }
 const Navbar = ({ onSearch }: Prop) => {
-  useEffect(() => {
-    document.title = "rawg-lite";
-  });
   return (
     <HStack m={1} p="10px">
       <Image src={icon} boxSize="60px" borderRadius="15px"></Image>
